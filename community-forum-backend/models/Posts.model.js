77f2714e817a5bea1a3f40f98c48aa4fd2937426
Tetsewa <amatwilliams@gmail.com>
2024-06-29 11:20:19 +0200

    updated post model

--- community-forum-backend/models/Posts.model.js
+++ community-forum-backend/models/Posts.model.js
@@ -1,10 +1,8 @@
-// models/userPosts.js
-
 const mongoose = require('mongoose');
-const { create } = require('./Event.model');
+// const { create } = require('./Event.model');
 const Schema = mongoose.Schema;
-
 const postsSchema = new Schema({
+  
   city: { type: String, required: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
@@ -13,7 +11,5 @@ const postsSchema = new Schema({
   contactInfo: { type: String, required: false },
  createdAt: { type: Date, default: Date.now, required: true },
 });
-
 const Posts = mongoose.model('Posts', postsSchema);
-
 module.exports = Posts;
