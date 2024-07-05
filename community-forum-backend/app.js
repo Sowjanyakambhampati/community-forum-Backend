152788b486921ba49f753ab0134b3e575de34233
Sowjanya Kambhampati <viswanath.chaitanya.kumar.kambhampati@ing.com>
2024-07-06 00:23:52 +0200

    nodemail updates

--- community-forum-backend/app.js
+++ community-forum-backend/app.js
@@ -11,15 +11,16 @@ const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.routes");
 const dashboardRoutes = require("./routes/dashboard.routes");
 const geoAPIRoutes = require('./routes/GeoAPI.routes');
+const nodemailer = require('nodemailer');
 
 mongoose
-  .connect(
-    "mongodb+srv://community-forum:0v44NdQ3C3RFLBye@community-forum-cluster.c0vfqhs.mongodb.net/?retryWrites=true&w=majority&appName=community-forum-cluster"
-  )
-  .then((x) =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch((err) => console.error("Error connecting to mongo", err));
+    .connect(
+        "mongodb+srv://community-forum:0v44NdQ3C3RFLBye@community-forum-cluster.c0vfqhs.mongodb.net/?retryWrites=true&w=majority&appName=community-forum-cluster"
+    )
+    .then((x) =>
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    )
+    .catch((err) => console.error("Error connecting to mongo", err));
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
@@ -27,10 +28,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/uploads", express.static("uploads"));
-
 
 
 app.use("/auth", authRoutes);
@@ -51,16 +51,42 @@ app.use('/posts/:city', postsRoutes);
 app.use('/:city/posts/:postId', postsRoutes);
 
 app.get('/geocode', async (req, res) => {
-  const { lat, lng } = req.query;
-  const apiKey = 'AIzaSyDlI0rjqyb4QaMqwbkKxunIhmZ4ZdBzFMY'; // Replace with your actual API key
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`;
+    const {lat, lng} = req.query;
+    const apiKey = 'AIzaSyDlI0rjqyb4QaMqwbkKxunIhmZ4ZdBzFMY'; // Replace with your actual API key
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`;
 
-  try {
-    const response = await axios.get(url);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching data' });
-  }
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching data'});
+    }
+});
+
+
+app.post('/send-email', async (req, res) => {
+    const transporter = nodemailer.createTransport(
+        {
+            host: 'smtp.example.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'info.buurthub@gmail.com',
+                pass: 'gtnyrirmubzfzngo',
+            },
+        }
+    );
+    transporter.sendMail(
+        {
+            to: 'viswanath16@gmail.com',
+            subject: 'My subject',
+            html: '<h1> Hi How are you</h1>'
+        }
+    ).then(() => {
+        console.log('Email sent');
+    }).catch((error) => {
+        console.error('Error sending email', error);
+    });
 });
 
 
