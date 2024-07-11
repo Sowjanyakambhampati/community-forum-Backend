ac8631c3eed56cb68f6865fe9fef022de922deba
Sowjanya Kambhampati <sowjanyaakella87@gmail.com>
2024-07-11 23:23:39 +0200

    reserve product node mailer setup

--- community-forum-backend/controllers/sendemail.controller.js
+++ community-forum-backend/controllers/sendemail.controller.js
@@ -28,6 +28,58 @@ class EmailController {
             console.error('Error sending email', error);
         });
     }
+
+    //Reserve product with image by node mailer
+    async reserveProduct(req, res) {
+        const transporter = nodemailer.createTransport(
+            {
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    user: 'info@buurthub.com',
+                    pass: 'gtnyrirmubzfzngo',
+                },
+            }
+        );
+        transporter.sendMail(
+            {
+                to: req.body.email,
+                subject: 'BuurtHub - Product Reservation notification !!',
+                html: '<h1> Hello,  Reservation successful and email sent!</h1>'
+            }
+        ).then(() => {
+            console.log('Email sent');
+        }).catch((error) => {
+            console.error('Error sending email', error);
+        });
 }
+
+//Reserve product with image by node mailer
+async reserveProduct(req, res) {
+    const transporter = nodemailer.createTransport(
+        {
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'info@buurthub.com',
+                pass: 'gtnyrirmubzfzngo',
+            },
+
+        );
+        transporter.sendMail(
+            {
+                to: req.body.email,
+                subject: 'BuurtHub - Product Reservation notification !!',
+                html: '<h1> Hello,  Reservation successful and email sent!</h1>'
+            }
+        ).then(() => {
+            console.log('Email sent');
+        }).catch((error) => {
+            console.error('Error sending email', error);
+        });
+}
+
 
 module.exports = new EmailController();
