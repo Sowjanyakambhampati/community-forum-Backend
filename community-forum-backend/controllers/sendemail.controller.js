955751cf6603f8b4122cceee9802e9ecefd2c325
Sowjanya Kambhampati <viswanath.chaitanya.kumar.kambhampati@ing.com>
2024-07-15 22:38:50 +0200

    Product update email controller added

--- community-forum-backend/controllers/sendemail.controller.js
+++ community-forum-backend/controllers/sendemail.controller.js
@@ -1,4 +1,3 @@
-
 const nodemailer = require("nodemailer");
 
 class EmailController {
@@ -29,32 +28,32 @@ class EmailController {
         });
     }
 
-//     //Reserve product with image by node mailer
-//     async reserveProduct(req, res) {
-//         const transporter = nodemailer.createTransport(
-//             {
-//                 host: 'smtp.gmail.com',
-//                 port: 465,
-//                 secure: true,
-//                 auth: {
-//                     user: 'info@buurthub.com',
-//                     pass: 'gtnyrirmubzfzngo',
-//                 },
-//             }
-//         );
-//         transporter.sendMail(
-//             {
-//                 to: req.body.email,
-//                 subject: 'BuurtHub - Product Reservation notification !!',
-//                 html: '<h1> Hello,  Reservation successful and email sent!</h1>'
-//             }
-//         ).then(() => {
-//             console.log('Email sent');
-//         }).catch((error) => {
-//             console.error('Error sending email', error);
-//         });
-// }
 
+    //Reserve product with image by node mailer
+    async sendEmailProdOwner(req, res) {
+        const transporter = nodemailer.createTransport(
+            {
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    user: 'info.buurthub@gmail.com',
+                    pass: 'gtnyrirmubzfzngo',
+                },
+            }
+        );
+        transporter.sendMail(
+            {
+                to: req.body.userEmail,
+                subject: 'BuurtHub - Product Reservation notification !!',
+                html: '<h1> Hello,  Your posted product on community forum has been registered!</h1>'
+            }
+        ).then(() => {
+            console.log('Email sent');
+        }).catch((error) => {
+            console.error('Error sending email', error);
+        });
+    }
 
 
 }
--- community-forum-backend/routes/email.routes.js
+++ community-forum-backend/routes/email.routes.js
@@ -4,4 +4,6 @@ const emailController = require("../controllers/sendemail.controller");
 
 router.post("/sendemail", emailController.sendEmail);
 
+router.post("/sendemail-product-owner", emailController.sendEmailProdOwner);
+
 module.exports = router;
